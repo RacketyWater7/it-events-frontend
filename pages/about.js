75000b43bb1dfb14211f1e2234f80d2ffabeafba
Haseeb <haseebbajwa1@outlook.com>
2021-05-30 15:16:52 +0500

    added pages and routes

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Section</h1>
+      <p>
+        This is an app to find the latest Information Technology related events
+      </p>
+      <p>Version: 1.0.0</p>
+    </div>
+  );
+}
---
+++ pages/events/add.js
@@ -0,0 +1,7 @@
+export default function AddEventPage() {
+  return (
+    <div>
+      <h1>Add a new Event</h1>
+    </div>
+  );
+}
