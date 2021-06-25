2b7aa9e6834b349423314da6de21d41be7d8e89f
Haseeb <haseebbajwa1@outlook.com>
2021-06-25 15:38:48 +0500

    created the layout

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,13 @@
+import Layout from "../components/Layout";
+
 export default function AboutPage() {
   return (
-    <div>
+    <Layout title="About IT Events">
       <h1>About Section</h1>
       <p>
         This is an app to find the latest Information Technology related events
       </p>
       <p>Version: 1.0.0</p>
-    </div>
+    </Layout>
   );
 }
